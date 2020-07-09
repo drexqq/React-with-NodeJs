fcde85b0b69fe394361949aeafa67bdac2504cac
drexqq <01037923530a@gmail.com>
2020-07-09 06:24:03 +0000

    load datas from database

--- src/components/TodoContext.js
+++ src/components/TodoContext.js
@@ -1,28 +1,5 @@
 import React, { useReducer, createContext, useContext, useRef } from "react";
 
-const initialTodos = [
-  {
-    id: 1,
-    text: "프로젝트 생성하기",
-    done: true,
-  },
-  {
-    id: 2,
-    text: "컴포넌트 스타일링",
-    done: true,
-  },
-  {
-    id: 3,
-    text: "Context 만들기",
-    done: false,
-  },
-  {
-    id: 4,
-    text: "기능 구현하기",
-    done: false,
-  },
-];
-
 // 만들 actions
 // Create
 // Toggle
@@ -35,11 +12,11 @@ function TodoRecuder(state, action) {
 
     case "TOGGLE":
       return state.map((todo) =>
-        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+        todo.__id === action.__id ? { ...todo, done: !todo.done } : todo
       );
 
     case "REMOVE":
-      return state.filter((todo) => todo.id !== action.id);
+      return state.filter((todo) => todo.__id !== action.__id);
 
     default:
       throw new Error(`Unhandled Action type: ${action.type}`);
@@ -49,11 +26,22 @@ function TodoRecuder(state, action) {
 const TodoStateContext = createContext();
 const TodoDispatchContext = createContext();
 const TodoNextIdContext = createContext();
-
 export function TodoProvider({ children }) {
+  async function load() {
+    let url = "http://3.23.219.141:5000/api/load";
+    await fetch(url, {
+      method: "POST",
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        return response.data;
+      });
+  }
+  console.log(load());
+  const initialTodos = [];
   const [state, dispatch] = useReducer(TodoRecuder, initialTodos);
-  const nextId = useRef(5);
-
+  const nextId = useRef(1);
   return (
     <TodoStateContext.Provider value={state}>
       <TodoDispatchContext.Provider value={dispatch}>
