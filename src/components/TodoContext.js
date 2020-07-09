70a20d6a62d9bb0d2b05a93b776897413264c8f7
drexqq <01037923530a@gmail.com>
2020-07-09 10:02:15 +0900

    todocontext modify

--- src/components/TodoContext.js
+++ src/components/TodoContext.js
@@ -1,26 +1,28 @@
-import React, { useReducer, createContext, useContext, useRef } from 'react';
-
+import React, { useReducer, createContext, useContext, useRef } from "react";
+const todoSchema = require("../../../models/todo");
+console.log(todoSchema);
 const initialTodos = [
-    {
-        id: 1,
-        text: '프로젝트 생성하기',
-        done: true,
-    },
-    {
-        id: 2,
-        text: '컴포넌트 스타일링',
-        done: true,
-    },
-    {
-        id: 3,
-        text: 'Context 만들기',
-        done: false,
-    },
-    {
-        id: 4,
-        text: '기능 구현하기',
-        done: false,
-    },
+  {},
+  {
+    id: 1,
+    text: "프로젝트 생성하기",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "컴포넌트 스타일링",
+    done: true,
+  },
+  {
+    id: 3,
+    text: "Context 만들기",
+    done: false,
+  },
+  {
+    id: 4,
+    text: "기능 구현하기",
+    done: false,
+  },
 ];
 
 // 만들 actions
@@ -29,21 +31,21 @@ const initialTodos = [
 // Remove
 
 function TodoRecuder(state, action) {
-    switch (action.type) {
-        case 'CREATE':
-            return state.concat(action.todo);
+  switch (action.type) {
+    case "CREATE":
+      return state.concat(action.todo);
 
-        case 'TOGGLE':
-            return state.map(todo =>
-                todo.id === action.id ? { ...todo, done: !todo.done } : todo,
-            );
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
 
-        case 'REMOVE':
-            return state.filter(todo => todo.id !== action.id);
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.id);
 
-        default:
-            throw new Error(`Unhandled Action type: ${action.type}`);
-    }
+    default:
+      throw new Error(`Unhandled Action type: ${action.type}`);
+  }
 }
 
 const TodoStateContext = createContext();
@@ -51,40 +53,40 @@ const TodoDispatchContext = createContext();
 const TodoNextIdContext = createContext();
 
 export function TodoProvider({ children }) {
-    const [state, dispatch] = useReducer(TodoRecuder, initialTodos);
-    const nextId = useRef(5);
+  const [state, dispatch] = useReducer(TodoRecuder, initialTodos);
+  const nextId = useRef(5);
 
-    return (
-        <TodoStateContext.Provider value={state}>
-            <TodoDispatchContext.Provider value={dispatch}>
-                <TodoNextIdContext.Provider value={nextId}>
-                    {children}
-                </TodoNextIdContext.Provider>
-            </TodoDispatchContext.Provider>
-        </TodoStateContext.Provider>
-    );
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
 }
 
 export function useTodoState() {
-    const context = useContext(TodoStateContext);
-    if (!context) {
-        throw new Error(`Cannot find TodoProvider`);
-    }
-    return context;
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error(`Cannot find TodoProvider`);
+  }
+  return context;
 }
 
 export function useTodoDispatch() {
-    const context = useContext(TodoDispatchContext);
-    if (!context) {
-        throw new Error(`Cannot find TodoDispatchContext`);
-    }
-    return context;
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error(`Cannot find TodoDispatchContext`);
+  }
+  return context;
 }
 
 export function useTodoNextId() {
-    const context = useContext(TodoNextIdContext);
-    if (!context) {
-        throw new Error(`Cannot find TodoNextIdContext`);
-    }
-    return context;
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error(`Cannot find TodoNextIdContext`);
+  }
+  return context;
 }
